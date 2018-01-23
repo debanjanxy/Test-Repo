ae5e90619d4da843a75c36a853965fce37bab397
debanjanxy <dasdebanjan624@gmail.com>
2018-01-23 15:21:53 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#!usr/bin/python
+
+if __name__=='main':
+    #login to moodle
+
+    #go to the specified course page
+
+    #get all the links in the page
+
+    #go to every link and find "submission file" part in the urls present in
+    #that page.
+
+    #Get the all such inner level urls and download all the files in a
+    #specified folder. 
